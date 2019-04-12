19b61a008142babf72d63e8fd73b3d6461f03ad9
Rahul Gopinath <rahul@gopinath.org>
2019-04-12 01:50:25 -0700

    x

--- src/refine.py
+++ src/refine.py
@@ -5,7 +5,24 @@ import sys
 def t(a): return ''.join([str(s) for s in a])
 def flatten(arr): return [i for x in arr for i in (flatten(x) if isinstance(x, (list, tuple)) else [x])]
 
-def to_string(arr): return ''.join([str_db.get(a, a) for a in arr])
+def tree_to_str(node, nt, expansion):
+    """Reconstruct the tree replacing nt with expansion"""
+    node, children, *_ = node
+    if node == nt: return expansion
+    else:
+        if not children: return node
+        else: return ''.join(tree_to_str(c, nt, expansion) for c in children)
+
+def to_string(nt, rule, tree):
+    """
+    Given a token, and its corresponding rule, first obtain the expansion
+    string by replacing all tokens with candidates, then reconstruct the string
+    from the derivation tree by recursively traversing and replacing any node
+    that corresponds to nt with the expanded string.
+    """
+    # could be an array depending on how many combinations we get for rule
+    expansion = ''.join([str_db.get(a, a) for a in rule]) 
+    return tree_to_str(tree, nt, expansion)
 
 import calc_parse_
 
@@ -21,43 +38,37 @@ def check(s):
         exec_map[s] = False
         return False
 
-def gen_alt(arr):
+def gen_alt(arr, start, end):
     sys.stdout.flush()
     my_lst = []
     length = len(arr)
     # alpha_1 != e and alpha_2 != e
-    if length:
-        if length == 1:
-            my_lst.append((arr, '%s' % ''.join(arr)))
-            return my_lst
-        elif length > 1:
-            my_lst.append((arr, '\(%s\)' % ''.join(arr)))
-        else:
-            assert False
     for i in range(1,length): # shorter alpha_1 prioritized
         alpha_1 = arr[:i]
         alpha_2 = arr[i:]
         assert alpha_1
         assert alpha_2
-        a1_rep = gen_rep(alpha_1)
-        a2_alt = gen_alt(alpha_2)
+        a1_rep = gen_rep(alpha_1, start=start, end=start+i-1)
+        a2_alt = gen_alt(alpha_2, start=start+i, end=start+len(arr)-1)
         for a1, a1_s in a1_rep:
             for a2, a2_s in a2_alt:
                 key = '\(%s\|%s\)' % (a1_s, a2_s)
                 my_lst.append((a1, key))
                 my_lst.append((a2, key))
+
+    if length: # this is the final choice.
+        if length == 1:
+            assert not my_lst
+            my_lst.append((arr, '%s' % ''.join(arr) ))
+            return my_lst
+        my_lst.append((arr, '\(%s\)' % ''.join(arr) ))
     return my_lst
 
 # returns a list.
-def gen_rep(arr):
+def gen_rep(arr, start, end):
     sys.stdout.flush()
     my_lst = []
     length = len(arr)
-    if length:
-        if length == 1:
-            my_lst.append((arr, '\(%s\)' % ''.join(arr)))
-        else:
-            my_lst.append((arr, '%s' % ''.join(arr)))
     for i in range(length): # shorter alpha1 prioritized
         alpha_1 = arr[:i]
         alpha_1_s = ''.join(alpha_1)
@@ -66,28 +77,34 @@ def gen_rep(arr):
             alpha_2 = arr[i:j]
             assert alpha_2
             alpha_3 = arr[j:]
-            a2_alt = gen_alt(alpha_2) # we need to check multiples 0, 1, 2.
-            a3_rep = gen_rep(alpha_3)
+            a2_alt = gen_alt(alpha_2, start=start+i, end=start+j-1) # we need to check multiples 0, 1, 2.
+            a3_rep = gen_rep(alpha_3, start=start+j, end=start+length-1)
             for n in [0, 1, 2]:
                 for a2, a2_s in a2_alt:
                     if a3_rep:
                         for a3, a3_s in a3_rep:
                             my_lst.append(((alpha_1, a2 * n, a3), '\(%s%s\*%s\)' % (alpha_1_s, a2_s, a3_s)))
                             if n != 0:
-                                my_lst.append(((alpha_1, a2 * n, a3), '\(%s%s\+%s\)' % (alpha_1_s, a2_s, a3_s)))
+                                my_lst.append(((alpha_1, a2 * n, a3), '\(%s%s\+%s\)' % (alpha_1_s, a2_s, a3_s) ))
                     else:
                         my_lst.append(((alpha_1, a2 * n), '\(%s%s\*\)' % (alpha_1_s, a2_s)))
                         if n != 0:
                             my_lst.append(((alpha_1, a2 * n), '\(%s%s\+\)' % (alpha_1_s, a2_s)))
+    if length: # the final choice
+        if length == 1:
+            my_lst.append((arr, '\(%s\)' % ''.join(arr)))
+        else:
+            my_lst.append((arr, '%s' % ''.join(arr)))
+
     return my_lst
 
 str_db = {}
 
 regex_map = {}
-def main(tree_file, rule, alt):
+def main(tree_file, nt, alt):
     tree = json.load(open(tree_file))
     grammar = get_grammar(tree)
-    if rule is None:
+    if nt is None:
         for i, k in enumerate(grammar):
             for j,a in enumerate(grammar[k]):
                 print(i,j,k, ' '.join(["%d:%s" % (i,t) for i,t in enumerate(a)]))
@@ -96,23 +113,23 @@ def main(tree_file, rule, alt):
     to_map(tree, str_db, grammar)
     #for i in str_db: print(i, str_db[i])
     sys.stdout.flush()
-    # the idea is as follows: We choose a single rule to refine, and a single
+    # the idea is as follows: We choose a single nt to refine, and a single
     # alternative at a time.
     # Then, consider that single alternative as a sting, with each token a
     # character. Then apply regular expression synthesis to determine the
     # abstraction candiates. Place each abstraction candidate as the replacement
-    # for that rule, and generate the minimum string. Evaluate and verify that
+    # for that nt, and generate the minimum string. Evaluate and verify that
     # the string is accepted (adv: verify that the derivation tree is
     # as expected). Do this for each alternative, and we have the list of actual
     # alternatives.
-    my_rule = grammar[rule]
+    my_rule = grammar[nt]
     my_alt = sorted(my_rule)[alt]
     print(my_alt, file=sys.stderr)
-    lres = gen_rep(my_alt)
+    lres = gen_rep(my_alt, start=0, end=len(my_alt)-1)
     #
     sys.stdout.flush()
     for l,s in lres:
-        expr = to_string(flatten(l))
+        expr = to_string(nt, flatten(l), tree)
         if regex_map.get(s, False):
             v = check(expr)
             regex_map[s] = v
@@ -138,4 +155,4 @@ def get_grammar(tree): return to_grammar.to_grammar(tree, {})
 
 import to_grammar
 if __name__ == '__main__':
-    main(sys.argv[1], rule=(sys.argv[2] if len(sys.argv) > 2 else None), alt=(int(sys.argv[3]) if len(sys.argv) > 3 else 0))
+    main(sys.argv[1], nt=(sys.argv[2] if len(sys.argv) > 2 else None), alt=(int(sys.argv[3]) if len(sys.argv) > 3 else 0))
