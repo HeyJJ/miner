473bfffc4f5a1da78078ac9aaac187f2c2e05861
Rahul Gopinath <rahul@gopinath.org>
2019-04-12 08:02:29 -0700

    to regexers

--- src/refine.py
+++ src/refine.py
@@ -43,7 +43,7 @@ def to_strings(nt, regex, tree):
     from the derivation tree by recursively traversing and replacing any node
     that corresponds to nt with the expanded string.
     """
-    for rule in regex_to_rules(regex):
+    for rule in regex.to_rules():
         arr = [list(str_db.get(token, [token])) for token in rule]
         for lst in itertools.product(*arr):
             expansion = ''.join(lst)
@@ -63,6 +63,55 @@ def check(s):
         exec_map[s] = False
         return False
 
+class Regex:
+    def to_rules(self):
+        if isinstance(self, Alt):
+            for a1 in self.a1.to_rules():
+                yield a1
+            for a2 in self.a2.to_rules():
+                yield a2
+        elif  isinstance(self, Rep):
+            for a3 in self.a.to_rules():
+                for n in [0, 1, 2]:
+                    yield a3 * n
+        elif  isinstance(self, Seq):
+            if not self.arr: return
+            if self.arr[0]:
+                for a4 in self.arr[0].to_rules():
+                    for a5 in Seq(self.arr[1:]).to_rules():
+                        yield a4 + a5
+                    if not self.arr[1:]:
+                        yield a4
+            else:
+                for a6 in Seq(self.arr[1:]).to_rules():
+                    yield a6
+        elif  isinstance(self, One):
+            assert not isinstance(One, Regex)
+            yield self.o
+        else:
+            assert False
+
+    def __str__(self):
+        if isinstance(self, Alt):
+            return "(%s|%s)" % (str(self.a1), str(self.a2))
+        elif  isinstance(self, Rep):
+            return "%s*" % self.a
+        elif  isinstance(self, Seq):
+            return "%s" % ''.join(str(a) for a in self.arr)
+        elif  isinstance(self, One):
+            return ''.join(str(o).replace('*', '[*]').replace('(', '[(]').replace(')', '[)]') for o in self.o)
+        else:
+            assert False
+
+class Alt(Regex):
+    def __init__(self, a1, a2): self.a1, self.a2 = a1, a2
+class Rep(Regex):
+    def __init__(self, a): self.a = a
+class Seq(Regex):
+    def __init__(self, arr): self.arr = arr
+class One(Regex):
+    def __init__(self, o): self.o = o
+
 def gen_alt(arr):
     length = len(arr)
     # alpha_1 != e and alpha_2 != e
@@ -72,62 +121,11 @@ def gen_alt(arr):
         assert alpha_2
         for a1 in gen_rep(alpha_1):
             for a2 in gen_alt(alpha_2):
-                yield alt(a1, a2)
+                yield Alt(a1, a2)
     if length: # this is the final choice.
-        yield arr
+        yield One(arr)
     return
 
-def alt(a1, a2): return (0, a1, a2)
-def rep(a): return (1, a)
-def seq(*arr): return tuple([2, *arr])
-
-
-def regex_to_rules(regex):
-    if regex[0] == 0: # alt
-        _, a, b = regex
-        for a1 in regex_to_rules(a):
-            yield a1
-        for b1 in regex_to_rules(b):
-            yield b1
-    elif regex[0] == 1: # rep
-        _, a = regex
-        for a3 in regex_to_rules(a):
-            for n in [0, 1, 2]:
-                yield a3 * n
-    elif regex[0] == 2: # seq
-        _, *arr = regex
-        if arr[0]:
-            for a4 in regex_to_rules(arr[0]):
-                if arr[1:]:
-                    for a5 in regex_to_rules((2, *arr[1:])):
-                        yield a4 + a5
-                else:
-                    yield a4
-        else:
-            for a5 in regex_to_rules((2, *arr[1:])):
-                yield a5
-
-    else:
-        yield regex
-
-
-def regex_to_string(regex):
-    if not regex: return ''
-    if len(regex) == 1: return regex[0]
-    elif regex[0] == 0: # alt
-        _, a, b = regex
-        return "\(%s\)" % (regex_to_string(a) + '\|' + regex_to_string(b))
-    elif regex[0] == 1: # rep
-        _, a = regex
-        return "\(%s\)" % (regex_to_string(a) + '\*')
-    elif regex[0] == 2: # seq
-        _, *arr = regex
-        if len(arr) == 1:
-            return "%s" % regex_to_string(arr[1])
-        else:
-            return "\(%s\)" % ''.join([regex_to_string(a) for a in arr])
-    else:
-        return "\(%s\)" % ''.join(regex)
 
 def gen_rep(arr):
     length = len(arr)
@@ -139,11 +137,11 @@ def gen_rep(arr):
             assert alpha_2
             for a2 in gen_alt(alpha_2):
                 for a3 in gen_rep(alpha_3):
-                    yield seq(alpha_1, rep(a2), a3)
+                    yield Seq([One(alpha_1), Rep(a2), a3])
                 if not alpha_3:
-                    yield seq(alpha_1, rep(a2))
+                    yield Seq([One(alpha_1), Rep(a2)])
     if length: # the final choice
-        yield arr
+        yield One(arr)
     return
 
 str_db = {}
@@ -175,12 +173,12 @@ def process_alt(nt, my_alt, tree):
                 if not v: break # one sample of regex failed. Exit
             all_true = True
         if all_true: # get the first
-            print("nt:", nt, 'rule:', regex_to_string(regex))
+            print("nt:", nt, 'rule:', str(regex))
             break
 
     for k in regex_map:
         if regex_map[k]:
-            print('->        ', regex_to_string(k))
+            print('->        ', str(k))
     print('')
     regex_map.clear()
     sys.stdout.flush()
